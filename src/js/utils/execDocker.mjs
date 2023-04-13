2b8919ff113c9d3717064a64eed4e3673e291fb2
Vimalnath Nambiar <vnathnambiar@hotmail.com>
2023-04-13 09:55:19 +0800

    added try-catch block

--- src/js/utils/execDocker.mjs
+++ src/js/utils/execDocker.mjs
@@ -8,9 +8,13 @@ import * as cmd from 'child_process';
  * Function Modules
  */
 export async function execDocker(param) {
-  const pwizImg = 'proteowizard/pwiz-skyline-i-agree-to-the-vendor-licenses';
-  let msConvert = `docker run --rm -v ${param.inputDir}:/inputDir -v ${param.outputDir}:/outputDir ${pwizImg} wine msconvert /inputDir/${param.sampleFile} -o /outputDir`;
+  try {
+    const pwizImg = 'proteowizard/pwiz-skyline-i-agree-to-the-vendor-licenses';
+    let msConvert = `docker run --rm -v ${param.inputDir}:/inputDir -v ${param.outputDir}:/outputDir ${pwizImg} wine msconvert /inputDir/${param.sampleFile} -o /outputDir`;
 
-  let output = cmd.execSync(`${msConvert}`);
-  console.log(output.toString());
+    let output = cmd.execSync(`${msConvert}`);
+    console.log(output.toString());
+  } catch (err) {
+    console.log(`${err}`);
+  }
 }
